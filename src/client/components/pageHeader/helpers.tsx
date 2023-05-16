7b0dd59db98228f00d858aafdae51ee66a02405e
danil136 <danil.makogonov123123136@gmail.com>
2023-05-16 17:25:57 +0300

    review fixes 2

--- src/client/components/pageHeader/helpers.tsx
+++ src/client/components/pageHeader/helpers.tsx
@@ -1,18 +1,20 @@
-import React from 'react'
+import React, { FC } from 'react'
 import { pathSet } from './types'
 import { HeaderArrow } from '~/common/icons/HeaderArrow'
 import { Link } from 'react-router-dom'
 
-export const NavItems = (path: pathSet) => {
+export const NavItems: FC<{path: pathSet}> = ({path}) => {
   const keys = Object.keys(path)
   let pathsTillCur = ''
-  return keys.map(key => {
-    pathsTillCur += `/${key}`
-    return <li key={key} className="news_header_links__item">
-      <HeaderArrow />
-      <Link to={pathsTillCur}>
-        <span>{key}</span>
-      </Link>
-    </li>
-  })    
+  return <>
+    {keys.map(key => {
+      pathsTillCur += `/${key}`
+      return <li key={key} className="news_header_links__item">
+        <HeaderArrow />
+        <Link to={pathsTillCur}>
+          <span>{key}</span>
+        </Link>
+      </li>
+    })}
+  </>
 }
--- src/client/components/pageHeader/index.tsx
+++ src/client/components/pageHeader/index.tsx
@@ -5,6 +5,7 @@ import './pageHeader.scss'
 import { Link } from 'react-router-dom'
 import { ClientRoutes } from '~/common/types/routes'
 import { Home } from '~/common/icons/Home'
+import { NavItems } from './helpers'
 
 const PageHeader: React.FC<Props> = ({path, header, description}) => {
 
@@ -16,15 +17,7 @@ const PageHeader: React.FC<Props> = ({path, header, description}) => {
             <Home/>
           </Link>
         </li>
-        {Object.keys(path).map((name: string, _, array) => {
-          const pathsTillCur = array.slice(0, array.indexOf(name) + 1).reduce((a: string, v: string) => a + '/' + path[v as PathKey], '')
-          return <li key={name} className="news_header_links__item">
-            <HeaderArrow />
-            <Link to={pathsTillCur}>
-              <span>{name}</span>
-            </Link>
-          </li>
-        })}
+        <NavItems path={path}/>
       </ul>
       <h1 className="news_header__title">{header}</h1>
       {description && <p className="news_header__desc">{description}</p>}
