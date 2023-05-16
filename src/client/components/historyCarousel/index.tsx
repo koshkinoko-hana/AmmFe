e191e87c3a68a5f90c898b348ab3278bf8fca61d
danil136 <danil.makogonov123123136@gmail.com>
2023-05-16 15:54:29 +0300

    review fixes

--- src/client/components/historyCarousel/index.tsx
+++ src/client/components/historyCarousel/index.tsx
@@ -1,38 +1,11 @@
 import './historyCarousel.scss'
 import { Props } from './types'
-import useWindowDimensions from '@common/hooks/useWindowDimensions'
 import React, { useRef, useState } from 'react'
 
 const HistoryCarousel: React.FC<Props> = ({points}) => {
 
   const ref = useRef<HTMLDivElement>(null)
-
-  const [ currentScrollPosition, setCurrentScrollPosition ] = useState(0)
-  const { width } = useWindowDimensions()
   const [ currentPoint, setCurrentPoint ] = useState(0)
-
-  // const onArrowClick = (forward: boolean) => {
-  //   let newPosition
-  //   if (!ref.current) return
-  //   if (forward) {
-  //     newPosition = currentScrollPosition + width
-  //     setCurrentSlide((slide) => slide + 1)
-  //     if (ref.current.clientWidth - width < newPosition) {
-  //       newPosition = ref.current.clientWidth - width
-  //       setCurrentSlide(points.length - 1)
-  //     }
-  //   } else {
-  //     newPosition = currentScrollPosition - width
-  //     setCurrentSlide((slide) => slide - 1)
-  //     if (newPosition < 0) {
-  //       newPosition = 0
-  //       setCurrentSlide(0)
-  //     }
-  //   }
-
-  //   ref.current?.scrollTo({ left: newPosition })
-  //   setCurrentScrollPosition(newPosition)
-  // }
 
   return (
     <div ref={ref} className="history-carousel">
@@ -46,7 +19,7 @@ const HistoryCarousel: React.FC<Props> = ({points}) => {
           ))}
         </ul>
         <div className="history-carousel_info">
-          <h3 className="history-carousel_info__title">{points[currentPoint].header}</h3>
+          <h4 className="history-carousel_info__title">{points[currentPoint].header}</h4>
           <p className="history-carousel_info__desc">{points[currentPoint].text}</p>
         </div>
       </div>
