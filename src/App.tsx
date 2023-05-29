f8c3b68c7852923c3f098283345c163dd908e99e
mariya.tikhonova <mariya.tikhonova@dsr-corporation.com>
2023-05-29 11:59:53 +0300

    resolved conflicts & problems

--- src/App.tsx
+++ src/App.tsx
@@ -15,37 +15,35 @@ import { AdminRoutes, ClientRoutes } from '@common/types/routes'
 import DepartmentsPage from './client/pages/departmentPage'
 import DepartmentPage from './client/pages/departments/Department1Page'
 import NewsDetailsPage from './client/pages/newsDetailsPage'
-import DepartmentsPage from './client/pages/departmentPage'
-import DepartmentPage from './client/pages/departments/Department1Page'
 import AdminDepartmentPage from './admin/pages/departmentPage'
 
 function App(): JSX.Element {
 
   return (
     <Routes>
-      <Route path={`/${AdminRoutes.root}/${AdminRoutes.login}`} element={<Login />} />
-      <Route path={`/${AdminRoutes.root}`} element={<AdminRoute />}>
-        <Route path={`${AdminRoutes.departments}`} element={<Departments />} />
-        <Route path={`${AdminRoutes.department}`} >
-          <Route path="" element={<AdminDepartmentPage />} />
-          <Route path="create" element={<AdminDepartmentPage />} />
-          <Route path=":id" element={<AdminDepartmentPage />} />
+      <Route path={`/${AdminRoutes.root}/${AdminRoutes.login}`} element={<Login/>}/>
+      <Route path={`/${AdminRoutes.root}`} element={<AdminRoute/>}>
+        <Route path={`${AdminRoutes.departments}`} element={<Departments/>}/>
+        <Route path={`${AdminRoutes.department}`}>
+          <Route path="" element={<AdminDepartmentPage/>}/>
+          <Route path="create" element={<AdminDepartmentPage/>}/>
+          <Route path=":id" element={<AdminDepartmentPage/>}/>
         </Route>
-        <Route path={`${AdminRoutes.positions}`} element={<Positions />} />
+        <Route path={`${AdminRoutes.positions}`} element={<Positions/>}/>
         <Route path={`${AdminRoutes.employees}`}>
-          <Route path="" element={<Employees />} />
-          <Route path="create" element={<Employee />} />
-          <Route path=":id" element={<Employee />} />
+          <Route path="" element={<Employees/>}/>
+          <Route path="create" element={<Employee/>}/>
+          <Route path=":id" element={<Employee/>}/>
         </Route>
       </Route>
       <Route path={'/'} element={<ClientRoute/>}>
-        <Route path={`${ClientRoutes.home}`} element={<MainPage/>} />
+        <Route path={`${ClientRoutes.home}`} element={<MainPage/>}/>
         <Route path={`${ClientRoutes.news}`} element={<NewsPage/>}/>
         <Route path={`${ClientRoutes.news}/:slug`} element={<NewsDetailsPage/>}/>
-        <Route path={`${ClientRoutes.deanFaq}`} element={<QuestionPage/>} />
-        <Route path={`${ClientRoutes.departments}`} >
-          <Route path="" element={ <DepartmentsPage/>} />
-          <Route path=":id" element={<DepartmentPage/>} />
+        <Route path={`${ClientRoutes.deanFaq}`} element={<QuestionPage/>}/>
+        <Route path={`${ClientRoutes.departments}`}>
+          <Route path="" element={<DepartmentsPage/>}/>
+          <Route path=":id" element={<DepartmentPage/>}/>
         </Route>
         {/* <Route path={`${ClientRoutes.positions}`} element={<Positions/>}/> */}
         {/*<Route path={`${ClientRoutes.employees}`}>*/}
