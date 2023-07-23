8a89a9f8ea25b3f25a854db4bd9f0e025e55a781
mariya.tikhonova <mariya.tikhonova@dsr-corporation.com>
2023-07-23 14:24:17 +0300

    фикс ссылок

--- src/common/consts/general.ts
+++ src/common/consts/general.ts
@@ -1,3 +1,3 @@
-const backendUrl = '172.18.0.3'
-export const apiAdmin = 'http://amm-be:3000/admin'
+const backendUrl = 'http://c1r011v05.amm.vsu.ru'
+export const apiAdmin = `${backendUrl}:3000/admin`
 export const apiClient = `${backendUrl}:3000`
